5f664f486dd3ebb210af6a457852071d741742b6
afzama <141961264+afzama@users.noreply.github.com>
2023-10-02 15:41:44 -0400

    logging user searched city

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const apiKey = "10216d4aaed49c4ee7392d040f6d98e3";
-const searchHistory = [];
+const searchHistory = localStorage.getItem('searchhistory')?.split(',') || [];
+const searchHistoryElement = document.getElementById("search-history");
 
+//fetch the weather for a searched city, add to search history, and display weather data
 function fetchWeatherData(cityName) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             console.log('Weather data:', data);
             updateWeatherInfo(data);
-            addToSearchHistory(cityName);
+            addToSearchHistory(data.name);
             displaySearchHistory();
         })
         .catch(error => console.log("Error fetching weather data: ", error));
 }
+
 function updateWeatherInfo(data) {
     document.getElementById("city-name").innerText = data.name;
     const todayDate = new Date(data.dt * 1000).toLocaleDateString();
@@ -25,26 +28,28 @@ function updateWeatherInfo(data) {
 function addToSearchHistory(cityName) {
     // Add the city to the search history array
     searchHistory.push(cityName);
+    localStorage.setItem('searchhistory', searchHistory);
 }
 
 function displaySearchHistory() {
     const searchHistoryElement = document.getElementById("search-history");
     if (searchHistoryElement) {
         searchHistoryElement.innerHTML = ""; // Clear previous content
+        // Display each city in the search history
+        searchHistory.forEach(city => {
+            const button = document.createElement("button");
+            button.innerText = city;
+            button.addEventListener("click", () => {
+                fetchWeatherData(city);
+            });
+            searchHistoryElement.appendChild(button);
+        });
     } else {
         console.error("Element with ID 'search-history' not found.");
     }
 }
 
-// Display each city in the search history
-searchHistory.forEach(city => {
-    const button = document.createElement("button");
-    button.innerText = city;
-    button.addEventListener("click", () => {
-        fetchWeatherData(city);
-    });
-    searchHistoryElement.appendChild(button);
-});
+//retrieve weather forecast data for next 5 days
 function displayFiveDayForecast(cityName) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
 
@@ -61,7 +66,7 @@ function displayFiveDayForecast(cityName) {
                 const date = new Date(forecastDays[i].dt * 1000).toLocaleDateString();
                 card.innerHTML = `
                     <div class="card-body" id="${dateId}">Date: ${date}</div>
-                    <h2 class="card-title">Card title</h2>
+                    <h2 class="selectcity">City: ${cityName}</h2>
                     <p class="card-text" id="temp">Temperature: ${forecastDays[i].main.temp}°C</p>
                     <p class="card-text" id="wind">Wind: ${forecastDays[i].wind.speed} m/s</p>
                     <p class="card-text" id="humid">Humidity: ${forecastDays[i].main.humidity}%</p>
@@ -71,19 +76,11 @@ function displayFiveDayForecast(cityName) {
         })
         .catch(error => console.log("Error fetching forecast data: ", error));
 }
-document.querySelector("button").addEventListener("click", function () {
+document.querySelector("#search-button").addEventListener("click", function () {
     const city = document.getElementById("citysearch").value;
     fetchWeatherData(city);
+    displayFiveDayForecast(city);
 });
 
 // Initial display of search history
 displaySearchHistory();
-
-// Event listener for the Search button
-document.querySelector("button").addEventListener("click", function () {
-    const city = document.getElementById("citysearch").value;
-    fetchWeatherData(city);
-});
-
-// Call fetchWeatherData function for an initial city (e.g., Atlanta)
-fetchWeatherData("Atlanta");
