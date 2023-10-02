28ac7d7c312d78efda1a51a21a735635c548b338
afzama <141961264+afzama@users.noreply.github.com>
2023-10-02 13:49:52 -0400

    apiupdate

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const apiKey = "10216d4aaed49c4ee7392d040f6d98e3";
 const searchHistory = [];
 
 function fetchWeatherData(cityName) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={10216d4aaed49c4ee7392d040f6d98e3}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+            console.log('Weather data:', data);
             updateWeatherInfo(data);
             addToSearchHistory(cityName);
             displaySearchHistory();
@@ -14,10 +15,11 @@ function fetchWeatherData(cityName) {
 }
 function updateWeatherInfo(data) {
     document.getElementById("city-name").innerText = data.name;
-    document.getElementById("date").innerText = "Date: " + new Date(data.dt * 1000).toLocaleDateString();
-    document.getElementById("temp").innerText = "Temperature: " + data.main.temp + "°C";
-    document.getElementById("wind").innerText = "Wind: " + data.wind.speed + " m/s";
-    document.getElementById("humid").innerText = "Humidity: " + data.main.humidity + "%";
+    const todayDate = new Date(data.dt * 1000).toLocaleDateString();
+    document.getElementById("date-0").innerText = "Date: " + new Date(data.dt * 1000).toLocaleDateString();
+    document.getElementById("temp-0").innerText = "Temperature: " + data.main.temp + "°C";
+    document.getElementById("wind-0").innerText = "Wind: " + data.wind.speed + " m/s";
+    document.getElementById("humid-0").innerText = "Humidity: " + data.main.humidity + "%";
 }
 
 function addToSearchHistory(cityName) {
@@ -27,19 +29,48 @@ function addToSearchHistory(cityName) {
 
 function displaySearchHistory() {
     const searchHistoryElement = document.getElementById("search-history");
-    searchHistoryElement.innerHTML = ""; // Clear previous content
-
-    // Display each city in the search history
-    searchHistory.forEach(city => {
-        const button = document.createElement("button");
-        button.innerText = city;
-        button.addEventListener("click", () => {
-            fetchWeatherData(city);
-        });
-        searchHistoryElement.appendChild(button);
-    });
+    if (searchHistoryElement) {
+        searchHistoryElement.innerHTML = ""; // Clear previous content
+    } else {
+        console.error("Element with ID 'search-history' not found.");
+    }
 }
 
+// Display each city in the search history
+searchHistory.forEach(city => {
+    const button = document.createElement("button");
+    button.innerText = city;
+    button.addEventListener("click", () => {
+        fetchWeatherData(city);
+    });
+    searchHistoryElement.appendChild(button);
+});
+function displayFiveDayForecast(cityName) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const forecastDays = data.list.filter(item => item.dt_txt.includes("12:00:00"));
+            const forecastContainer = document.getElementById("forecast-cards");
+
+            for (let i = 0; i < forecastDays.length; i++) {
+                const card = document.createElement("div");
+                card.classList.add("card");
+                const dateId = `date-${i + 1}`;
+                const date = new Date(forecastDays[i].dt * 1000).toLocaleDateString();
+                card.innerHTML = `
+                    <div class="card-body" id="${dateId}">Date: ${date}</div>
+                    <h2 class="card-title">Card title</h2>
+                    <p class="card-text" id="temp">Temperature: ${forecastDays[i].main.temp}°C</p>
+                    <p class="card-text" id="wind">Wind: ${forecastDays[i].wind.speed} m/s</p>
+                    <p class="card-text" id="humid">Humidity: ${forecastDays[i].main.humidity}%</p>
+                `;
+                forecastContainer.appendChild(card);
+            }
+        })
+        .catch(error => console.log("Error fetching forecast data: ", error));
+}
 document.querySelector("button").addEventListener("click", function () {
     const city = document.getElementById("citysearch").value;
     fetchWeatherData(city);
